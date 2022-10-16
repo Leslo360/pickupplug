6ab2cd79ae4632ad0efd87cbb946309f38301cfb
Lesego Sebusi <lesego@darkpools.co.za>
2022-10-16 21:18:24 +0200

    Cache_v1

--- next.config.js
+++ next.config.js
@@ -14,4 +14,24 @@ module.exports = withPWA({
   },
 });
 
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+
+module.exports = {
+  webpack: (config) => {
+    config.plugins.push(
+      new SWPrecacheWebpackPlugin({
+        minify: true,
+        staticFileGlobsIgnorePatterns: [/\.next\//],
+        runtimeCaching: [
+          {
+            handler: "networkFirst",
+            urlPattern: /^https?.*/,
+          },
+        ],
+      })
+    );
+    return config;
+  },
+};
+
 module.exports = nextConfig;
