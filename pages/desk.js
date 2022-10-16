af617b700c5c4f56ae83d6ac268136722a109ca2
Lesego Sebusi <lesego@darkpools.co.za>
2022-10-16 23:04:10 +0200

    Logic fix

--- pages/desk.js
+++ pages/desk.js
@@ -3,16 +3,19 @@ import React, { useState } from "react";
 import { IoIosRocket } from "react-icons/io";
 
 function Desk() {
+  const [hide, setHide] = useState(false);
   const [over21, setOver21] = useState(false);
   const [invalid, setInvalid] = useState(false);
 
   const validateAge = () => {
     setTimeout(() => {
+      setHide(true);
       setOver21(true);
     }, 800);
   };
   const invalidateAge = () => {
     setTimeout(() => {
+      setHide(true);
       setInvalid(true);
     }, 800);
   };
@@ -20,29 +23,28 @@ function Desk() {
   return (
     <div className="flex flex-col items-center">
       <Image alt="logo" width={200} height={200} src="/2.svg" />
-      {!over21 ||
-        (!invalid && (
-          <div className="flex flex-col items-center mt-32">
-            <h2>Welcome</h2>
-            <p className="text-sm w-2/4 text-center">Are you 21 or older?</p>
-            <div className="flex">
-              <button
-                onClick={() => invalidateAge()}
-                className="p-2 bg-none focus:animate-ping border-white border text-white m-3 text-sm flex items-center justify-between"
-              >
-                {/* <IoIosRocket color="white" className="mr-3 " size="20px" /> */}
-                Not yet
-              </button>
-              <button
-                onClick={() => validateAge()}
-                className="p-2 bg-none transition ease-linear focus:bg-white focus:text-black border-white border text-white m-3 text-sm flex items-center justify-between"
-              >
-                {/* <IoIosRocket color="white" className="mr-3 " size="20px" /> */}
-                Yup!
-              </button>
-            </div>
+      {!hide && (
+        <div className="flex flex-col items-center mt-32">
+          <h2>Welcome</h2>
+          <p className="text-sm w-2/4 text-center">Are you 21 or older?</p>
+          <div className="flex">
+            <button
+              onClick={() => invalidateAge()}
+              className="p-2 bg-none focus:animate-ping border-white border text-white m-3 text-sm flex items-center justify-between"
+            >
+              {/* <IoIosRocket color="white" className="mr-3 " size="20px" /> */}
+              Not yet
+            </button>
+            <button
+              onClick={() => validateAge()}
+              className="p-2 bg-none transition ease-linear focus:bg-white focus:text-black border-white border text-white m-3 text-sm flex items-center justify-between"
+            >
+              {/* <IoIosRocket color="white" className="mr-3 " size="20px" /> */}
+              Yup!
+            </button>
           </div>
-        ))}
+        </div>
+      )}
       {over21 && (
         <div className="flex flex-col items-center mt-32">
           <h2>Thought you were off the hook?</h2>
