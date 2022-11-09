a7afca1fee34397e9355d5673189f8b5e5e3f2e6
Lesego Sebusi <lesego@darkpools.co.za>
2022-11-09 17:33:13 +0200

    Bg Update

--- app/layout.js
+++ app/layout.js
@@ -3,26 +3,9 @@
 import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import "./globals.css";
-import LogoGif from "../images/logoDark.gif";
+import LogoGif from "../images/2.svg";
 
 export default function RootLayout({ children }) {
-  // useEffect(() => {
-  //   if ("serviceWorker" in navigator) {
-  //     window.addEventListener("load", function () {
-  //       navigator.serviceWorker.register("/sw.js").then(
-  //         function (registration) {
-  //           console.log(
-  //             "Service Worker registration successful with scope: ",
-  //             registration.scope
-  //           );
-  //         },
-  //         function (err) {
-  //           console.log("Service Worker registration failed: ", err);
-  //         }
-  //       );
-  //     });
-  //   }
-  // }, []);
   return (
     <html>
       <head>
@@ -32,7 +15,7 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
-        <div className="flex flex-col items-center content-center justify-between">
+        <div className="flex flex-col items-center content-center justify-between ">
           <Image
             alt="Pickup Plug Logo Gif"
             width={200}
